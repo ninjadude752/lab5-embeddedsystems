43caf9d915a09b09cd2d6ffd05764c1b5e66759f
iynix-xinyi <112117461+iynix-xinyi@users.noreply.github.com>
2023-04-10 17:04:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Lab5.c
+ * Lab5Embbeded.c
  *
  * Created: 4/10/2023 2:50:09 PM
  * Author : xyuan13
@@ -8,11 +8,30 @@
 #ifndef F_CPU
 #define F_CPU 16000000UL	// 16 MHz clock speed
 #endif
+#define FOSC 16000000
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
-	DDRC = 0b00110000;		// configure PC4(SDA), PC5(SCL) as output, other pins as inputs
+	
+	USART_Init(MYUBRR);
+	return 0;
+	
+ void USART_Init(unsigned int ubrr) {
+	/*set baud rate*/
+	UBRR0H = (unsigned char) (ubrr>>8);
+	UBRR0L = (unsigned char) ubrr;
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+	/* Set frame format: 8 data, 2 stop bit */
+	UCSR0C = (1<<USBS0) | (3<<UCSZ00);
+}
+	
+	
+	//DDRC = 0b00110000;		// configure PC4(SDA), PC5(SCL) as output, other pins as inputs
 	
 	/*
 	unsigned char c;
