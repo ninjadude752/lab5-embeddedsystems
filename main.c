6aa2d134a11a303795af6f91131b4b0765778546
iynix-xinyi <112117461+iynix-xinyi@users.noreply.github.com>
2023-04-11 16:23:10 -0500

    print hello world in arduino

--- main.c
+++ main.c
@@ -14,10 +14,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void)
+void USART_Init(unsigned int ubrr);
+void USART_Transmit(unsigned char data);
+unsigned char USART_Receive(void);
+
+int main()
 {	
 	DDRC = 0b00110000;		// configure PC4(SDA), PC5(SCL) as output, other pins as inputs(potentiometer connected to PC0)
 	USART_Init(MYUBRR);
+	unsigned char data[] = "hello world\n";
+	
+	ADCSRA = 0b10000111;		// enable ADC, set pre-scaler
+	ADMUX = 0b01000000;			// AVcc with external capacitor at AREF pin
+	ADCSRA = 0b11000111;
+	while (ADCSRA == 0b11000111);	// wait until the second bit is low
+	float high = ADCH;
+	float low = ADCL;
+	
+	while(1){
+		int i = 0;
+		while (data[i] != 0) {
+			USART_Transmit(data[i]);
+			i = i + 1;
+		}
+		USART_Receive();
+	}
 	return 0;
 }
  void USART_Init(unsigned int ubrr) {
@@ -28,6 +49,17 @@ int main(void)
 	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
 	/* Set frame format: 8 data, 2 stop bit */
 	UCSR0C = (1<<USBS0) | (3<<UCSZ00);
+}
+
+void USART_Transmit(unsigned char data) {
+	while (!(UCSR0A & (1<<UDRE0))) ;
+	UDR0 = data;
+}
+
+unsigned char USART_Receive(void) {
+	while (!(UCSR0A & (1<<RXC0))) ;
+	
+	return UDR0;
 }
 	
 	
