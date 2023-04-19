aeee96379f2a55f5c5ca5eda762c904ec18cb442
iynix-xinyi <112117461+iynix-xinyi@users.noreply.github.com>
2023-04-19 16:50:48 -0500

    finished

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #ifndef F_CPU
 #define F_CPU 16000000UL	// 16 MHz clock speed
 #endif
+#define __DELAY_BACKWARD_COMPATIBLE__
+
 #define FOSC 16000000
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
@@ -35,17 +37,19 @@ int main()
 	unsigned char str[20];
 	unsigned char readInArr[10];
 	unsigned char dacChannel[] = "DAC channel ";
-	unsigned char generate[] = "Generating ";
-	unsigned char sineWave[] = " sine wave cycles with f = ";
 	unsigned char hz[] = " Hz on DAC channel ";
 	unsigned char v[5];			// voltage value
-	unsigned char f[3];			// frequency value
+	unsigned char f[2];			// frequency value
 	unsigned char r[4];
+	int sinWave[] = {128, 141, 153, 165, 177, 188, 199, 209, 219, 227, 234, 241, 246, 250, 254, 255, 255, 255, 254, 250, 246, 241, 234, 227, 219, 209, 199, 188, 177, 165, 153, 141, 128, 115,
+					 103, 91, 79, 68, 57, 47, 37, 29, 22, 15, 10, 6, 2, 1, 0, 1, 2, 6, 10, 15, 22, 29, 37, 47, 57, 68, 79, 91, 103, 115};
+	int delayTable[] = {1250,1150,1050,950,850,750,650,550,450,350,250,150};
 	ADCSRA = 0b10000111;		// enable ADC, set pre-scaler
 	ADMUX = 0b01000000;			// AVcc with external capacitor at AREF pin
 	float high;
 	float Vin;
-	
+	int delay = 250;
+	int select = 0;
 
 	i2c_init();
 	
@@ -86,7 +90,7 @@ int main()
 				USART_Transmit(newLine[i]);
 				i = i + 1;
 			}
-			strcpy(str, "\0");
+			strcpy(str, "\n");
 			strcpy(readInArr, "\0");		
 		}
 		
@@ -99,10 +103,10 @@ int main()
 						strncat(dacChannel, &temp, 1);
 						// printing
 						int i = 0;
-						while (dacChannel[i] != 0) {			// print DAC 1 or DAC 0
+						for (i = 0; i < strlen(dacChannel); ++i) {
 							USART_Transmit(dacChannel[i]);
-							i = i + 1;
 						}
+						
 						unsigned char space = ' ';
 						USART_Transmit(space);
 						int count;
@@ -111,30 +115,32 @@ int main()
 							v[count] = readInArr[count+4];
 						}
 						
-						i = 0;
-						while (line[i] != 0) {					// print new line
+						
+						for (i = 0; i < strlen(line); ++i) {
 							USART_Transmit(line[i]);
-							i = i + 1;
 						}
 						
 						
 						// I2C: setting output voltage
 						// send start condition
-						float value = atof(v);
+						double value = atof(v);
 						value = value / 5 * 256;
 						i2c_start(slaveAddr);
-						if (readInArr == '0') {
+						if (readInArr[2] == '0') {
 							// command byte
 							i2c_write(0x00);			// set DAC0 output
+							//USART_Transmit(readInArr[2]);
 						}
-						else {
-							i2c_write(1);			// set DAC1 output
+						else if (readInArr[2] == '1') {
+							i2c_write(0x01);			// set DAC1 output
 						}
 						// output byte
-						i2c_write(value);					
+						i2c_write(value);			
 						i2c_stop();
 						
-						strcpy(v, "\n");							// reset v
+						for (i = 0; i < 5; ++ i) {
+							v[i] = '\n';
+						}
 						strcpy(dacChannel, "DAC channel ");		// reset dacChannel
 						
 					}
@@ -148,52 +154,90 @@ int main()
 			if (readInArr[1] == ',') {
 				if (readInArr[2] == '1' || readInArr[2] == '0') {
 					if (readInArr[3] == ',') {
-						f[0] = readInArr[4];
-						f[1] = readInArr[5];
 						if (readInArr[6] == ',') {
-							int count = 7;
-							int temp = 0;
-							while (readInArr[count] != '\0') {
-								r[temp] = readInArr[count];
-								temp = temp + 1;
-								count = count + 1;
-							}
+							f[0] = readInArr[4];
+							f[1] = readInArr[5];
 							
-							int i = 0;
-							while (generate[i] != 0) {
+							int i = 0;						
+							unsigned char generate[] = "Generating ";
+							for (i = 0; i < strlen(generate); ++i) {
 								USART_Transmit(generate[i]);
+							}
+							/*
+							i = 7;
+							int j = 0;
+							while (readInArr[i] != 0) {					// number of waveform cycles 
+								r[j] = readInArr[i];
+								USART_Transmit(readInArr[i]);
 								i = i + 1;
+								j = j + 1;
+							}
+							*/
+							
+							for (i = 7; i < 10; ++i) {
+								if(readInArr[i] == 'v'){
+									break;
+								}
+								else{
+									r[i-7] = readInArr[i];
+									USART_Transmit(readInArr[i]);	
+								}
+								
 							}
 							
-							i = 0;
-							while (r[i] != '\0') {					// number of waveform cycles 
-								USART_Transmit(r[i]);
-								i = i + 1;
-							}
-							
-							i = 0;
-							while (sineWave[i] != 0) {				
+							int numWave = (int)atof(r);
+						
+							unsigned char sineWave[] = " sine wave cycles with f = ";						
+							for (i = 0; i < strlen(sineWave); ++i) {
 								USART_Transmit(sineWave[i]);
-								i = i + 1;
 							}
 							
 							USART_Transmit(f[0]);					// frequency of the waveform
 							USART_Transmit(f[1]);
+							float frequency = atof(f);
 							
-							i = 0;
-							while (hz[i] != 0) {
+							for (i = 0; i < strlen(hz); ++i) {
 								USART_Transmit(hz[i]);
-								i = i + 1;
 							}
+										
 							
 							USART_Transmit(readInArr[2]);			// channel number
 							
-							i = 0;
-							while (line[i] != 0) {					// print new line
+							for (i = 0; i < strlen(line); ++i) {	// print new line
 								USART_Transmit(line[i]);
-								i = i + 1;
 							}
+							
+							
+							for (int j = 0; j < numWave; ++j) {
+								for (i = 0; i < 64; ++i) {
+									// I2C: generating sine wave
+									// send start condition
+									i2c_start(slaveAddr);
+									if (readInArr[2] == '0') {
+										// command byte
+										i2c_write(0x00);			// set DAC0 output
+									}
+									else if (readInArr[2] == '1') {
+										i2c_write(0x01);			// set DAC1 output
+									}
+									
+									// output byte
+									
+									i2c_write(sinWave[i]);
+									
+									_delay_ms((1/frequency)/64 * (800 - (frequency-10) * 18.56));	
+									//_delay_ms(0.48);
+									i2c_stop();
+									//1/20/64*x = 0.48 614.4
+									//1/10/64*x = 1.25 800
+			
+									
 
+									
+								}
+								//USART_Transmit(readInArr[2]);
+							}
+														
 						}
 					}
 				}
